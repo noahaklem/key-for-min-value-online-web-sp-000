5617b3a5aad8a419d181af46bef4ceb99725b57c
Noah  Klem <klem.noah@gmail.com>
2021-06-01 03:32:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,12 +9,7 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value|
     if lowest_value == 0 || value < lowest_value
-      #lowest_value = 0 still
-      #second pass lowest value = 0
-      #third pass lowest value = 0
       lowest_value = value
-      #lowest_key = brett
-      #second pass lowest_key = ashley
       lowest_key = key
     end
   end
