5835de6981dad956cc8a112abf32bee3fbfc1d58
Noah  Klem <klem.noah@gmail.com>
2021-06-01 03:25:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -20,9 +20,10 @@ def key_for_min_value(name_hash)
     if lowest_value == 0 || value < lowest_value
       #lowest_value = 0 still
       #second pass lowest value = 0
+      #third pass lowest value = 0
       lowest_value = value
       #lowest_key = brett
-      #second pass lowest_key = 
+      #second pass lowest_key = ashley
       lowest_key = key
     end
   end
